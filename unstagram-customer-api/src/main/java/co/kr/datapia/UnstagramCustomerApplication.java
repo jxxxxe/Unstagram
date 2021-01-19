0a5310b7b51dba33a0a133c42a6444f2412318f6
Jeongeun <jeun318@gmail.com>
2021-01-19 17:47:03 +0900

    21.01.09 초기설정

---
+++ unstagram-customer-api/src/main/java/co/kr/datapia/UnstagramCustomerApplication.java
@@ -0,0 +1,12 @@
+package co.kr.datapia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnstagramCustomerApplication
+{
+    public static void main(String[] args) {
+        SpringApplication.run(UnstagramCustomerApplication.class, args);
+    }
+}
---
+++ unstagram-customer-api/src/test/java/co/kr/datapia/UnstagramCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.datapia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UnstagramCustomerApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
